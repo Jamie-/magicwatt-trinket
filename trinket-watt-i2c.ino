89ea99286633e25c8e411ba90a36a5ea0613ebd0
Jamie Scott <git@jami.org.uk>
2017-04-23 19:34:55 +0100

    Cleaned up a little bit

--- trinket-watt-i2c.ino
+++ trinket-watt-i2c.ino
@@ -2,10 +2,13 @@
 #include "EmonLib.h"
 #include <math.h>
 
+// I2C bus address
 #define I2C_SLAVE_ADDRESS 0x04
 
-volatile byte dat;
+// Pin used for the current sensing circuitry.
+int currentPin = 2;
 
+volatile byte dat;
 EnergyMonitor em;
 
 void onRequest()
@@ -18,13 +21,13 @@ void setup()
     TinyWireS.begin(I2C_SLAVE_ADDRESS);
     TinyWireS.onRequest(onRequest);
 
-    em.current(2, 60.6);
+    em.current(currentPin, 60.6);
 }
 
 void loop()
 {
     uint16_t i = round(em.calcIrms(1480));
-    dat = lowByte(i);
+    dat = lowByte(i); // Gives a maximum usable value of 256 amps
     //if (i > 512) dat = 0xFF;
 
     TinyWireS_stop_check();
